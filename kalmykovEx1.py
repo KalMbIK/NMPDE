02bc164cf8220fd3c9c1b71332692cda766c6297
Denis Kalmykov <dekalmykov7@gmail.com>
2017-10-23 16:34:58 +0200

    InitCommit

---
+++ kalmykovEx1.py
@@ -0,0 +1,45 @@
+import numpy as np
+from math import factorial, pow
+
+class DiffApproxGenerator(object):
+    def __init__(self, m, p, q):
+        self.leftBorder = p
+        self.rightBorder = q
+        self.order = m
+        self.grid = np.arange(-p, q+1)
+        self.size = np.size(self.grid)
+        if m >= self.size:
+            raise AttributeError("input parameters p, q should "
+                                 "correspond to sufficiently many points to support an approximation")
+        self.vTaylor = np.vectorize(self.taylor)
+        self.tTaylor = self.generateTaylorTable()
+        self.coeffs = self.generateCoeffs()
+
+    def getGrid(self):
+        return self.grid
+
+    def taylor(self, k_, m_):
+        return pow(k_,m_)/factorial(m_)
+
+    def generateTaylorTable(self):
+        temp, gr = [], self.getGrid()
+        for i in range(self.size):
+            temp.append(self.vTaylor(gr,i))
+        return np.array(temp)
+
+    def generateCoeffs(self):
+        b = np.zeros(self.size)
+        b[self.order] = 1
+        return np.linalg.solve(self.tTaylor,b)
+
+    def findAccuracy(self):
+
+        pass
+
+
+symmCase = DiffApproxGenerator(1,0,6)
+print symmCase.size
+print symmCase.getGrid()
+# print symmCase.tTaylor
+print "Coeffs: " + str(symmCase.coeffs)
+print "Accuracy: " + str(symmCase.size-1)
